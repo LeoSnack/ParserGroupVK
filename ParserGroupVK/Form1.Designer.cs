3d0b6428e01e4402b825b8b2b90ff37fe2442b94
Leo <lev.kekish@gmail.com>
2019-11-20 17:14:08 +0300

    Commit
    Commit

---
+++ ParserGroupVK/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace ParserGroupVK
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Старт";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(12, 80);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(581, 218);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(364, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Осталось пройти:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(149, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(65, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(93, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Members";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(241, 17);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "View";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(277, 12);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(65, 20);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(149, 39);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(65, 20);
+            this.textBox4.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(123, 42);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "От";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(249, 46);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(22, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "До";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(277, 42);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(65, 20);
+            this.textBox5.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(605, 310);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "PaserDoors";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox5;
+    }
+}
+
---
+++ ParserGroupVK/Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using VkNet;
+using System.Windows.Forms;
+using VkNet.Enums.SafetyEnums;
+using VkNet.Model.RequestParams;
+using VkNet.Enums.Filters;
+using VkNet.Model;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Threading;
+using System.IO;
+
+namespace ParserGroupVK
+{
+    public partial class Form1 : Form
+    {
+
+        VkApi _api = new VkApi();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int CountView;
+        int CountMembersP;
+        int startID;
+        int resCheck;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public string StatuMembersWork(string GroupId)
+        {
+            var members = _api.Groups.GetById(null, GroupId, GroupsFields.MembersCount);
+            
+            if (members[0].Name == "DELETED" || members[0].IsClosed.ToString() == "Private" || members[0].Deactivated != null || members[0].IsClosed.ToString() == "Closed")
+            {
+                return "";
+            }
+            else
+            {
+                int countMem = members[0].MembersCount.Value;
+                string isClosed = members[0].IsClosed.ToString();
+                return countMem.ToString() + ":" + isClosed;
+            }
+        }
+        int chStep;
+        public void GetInfoGroup(string GroupId)
+        {
+            chStep = 3;
+            string MemStat = StatuMembersWork(GroupId);
+
+            Thread.Sleep(3000);
+
+            if (MemStat.Length > 0)
+            {
+                int CountMembers = Int32.Parse(MemStat.Split(':')[0]);
+                string isClosed = MemStat.Split(':')[1];
+                
+                if (isClosed == "Public")
+                {
+                    try
+                    {
+                        var get = _api.Wall.Get(new WallGetParams
+                        {
+                            Filter = WallFilter.Owner,
+                            OwnerId = long.Parse("-"+GroupId),
+                            Extended = true
+                        });
+
+                        Thread.Sleep(3000);
+
+                        if (get != null && get.WallPosts.Count > 0 && CountMembers < CountMembersP)
+                        {
+                            for (int countPost = 0; countPost < get.WallPosts.Count; countPost++)
+                            {
+                                if (get.WallPosts[countPost].Views != null && get.WallPosts[countPost].Views.Count > CountView)
+                                {
+                                    chStep = 1;
+                                    break;
+                                }
+                                else
+                                {
+                                    chStep = 0;
+                                }
+                            }
+                        }
+
+                        if (chStep == 1)
+                        {
+                            Log("club" + GroupId + " ПОДХОДИТ");
+                        }
+                    }
+                    catch
+                    {
+
+                    }
+                    //else if (chStep == 0)
+                    //    {
+                    //        Log("club" + GroupId + " НЕ подходит");
+                    //    }
+                    }
+                }
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            (sender as System.Windows.Forms.Button).Enabled = false;
+            auth();
+
+            CountView = Int32.Parse(textBox3.Text);
+            CountMembersP = Int32.Parse(textBox2.Text);
+
+            startID = Int32.Parse(textBox4.Text);
+            resCheck = Int32.Parse(textBox5.Text);
+
+            int countCheck = resCheck - startID;
+
+            var taskList = new List<Task>();
+
+            for (int i = 0; i < 1; i++)
+            {
+                taskList.Add(Task.Run(() =>
+                {
+                    for (int idG = startID; idG < resCheck; idG++)
+                    {
+                        GetInfoGroup(idG.ToString());
+                        label1.BeginInvoke(new InvokeDelegate(StatusWork));
+                        statusWork = "Осталось пройти: " + countCheck--.ToString();
+
+                        //WriteFile(mutex, idG);
+                    }
+                }));
+            }
+
+            await Task.WhenAll(taskList);
+            (sender as System.Windows.Forms.Button).Enabled = true;
+        }
+
+        public void Log(string value)
+        {
+            if (textBox1.InvokeRequired)
+            {
+                textBox1.Invoke(new Action<string>(Log), value);
+                return;
+            }
+            textBox1.Text += value + Environment.NewLine;
+        }
+
+        public void auth()
+        {
+            string email = "leva.kekish@mail.ru";
+            string pass = "iVK_8973!";
+            _api.Authorize(new ApiAuthParams
+            {
+                ApplicationId = 6921307,
+                Login = email,
+                Password = pass,
+                Settings = Settings.All
+            });
+        }
+
+        public delegate void InvokeDelegate();
+        string statusWork;
+        public void StatusWork()
+        {
+            label1.Text = statusWork;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        object mutex = new object();
+        public void WriteFile(object mutex, int value)
+        {
+            lock (mutex)
+            {
+                using (var fs = new FileStream(@"D:/id.txt", FileMode.Append, FileAccess.Write, FileShare.Read))
+                {
+                    using (var sw = new StreamWriter(fs))
+                    {
+                        sw.WriteLine(value.ToString());
+                    }
+                }
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
